2771c5db472f7d7959d2f68f7415f7e9f58a0a8f
JPFranca <135433891+jpfranca-br@users.noreply.github.com>
2024-12-09 17:05:19 -0300

    Create update_and_rebuild.sh

---
+++ update_and_rebuild.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Variables - local files
+SRC_DIR="$HOME/printer-server"
+
+# Function to check the last command's status
+check_status() {
+    if [ $? -ne 0 ]; then
+        echo "Error: $1"
+        exit 1
+    fi
+}
+
+# Install Go Dependencies
+echo "Installing Go dependencies..."
+go mod tidy
+check_status "Failed to install Go dependencies."
+
+# Build printer-server
+cd "$SRC_DIR"
+go build -o printer-server main.go
+check_status "Failed to build printer-server. Please check manually: go build -o printer-server main.go"
+
+# Build user-manager
+cd "$SRC_DIR"
+go build -o user-manager user-manager.go
+check_status "Failed to build user-manager. Please check manually: go build -o user-manager user-manager.go"
+    
+# Reloading daemon
+echo "Reloading daemon..."
+sudo systemctl daemon-reload
+
+# Restart and enable printer-server
+echo "Starting printer-server..."
+sudo systemctl restart printer-server
+check_status "Failed to (re)start printer-server. Please check manually."
+sudo systemctl enable printer-server
+check_status "Failed to enable printer-server at startup."
+
+# Finish
+echo "Update completed"
+echo "It is a good idea to run: sudo apt-get upgrade -y"
